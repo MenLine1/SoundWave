72cf3a9a2c91999279a88df6ef27ec7c14dcdbe6
MenLine1 <mmartyna790@gmail.com>
2024-05-18 13:56:16 +0200

    c

--- app/(site)/page.tsx
+++ app/(site)/page.tsx
@@ -9,15 +9,13 @@ export default async function Home() {
   const songs = await getSongs();
 
   return (
-      <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-hidden">
+      <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
         <Header>
             <div className="mb-2">
                 <h1 className="text-3xl font-bold text-white">
                     Welcome to SoundWave
                 </h1>
-                <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3
-                    2xl:grid-cols-4 gap-3 mt-4"
-                >
+                <div className="grid grid-cols-1 sm:grid-cols-2 xl:grid-cols-3 2xl:grid-cols-4 gap-3 mt-4">
                     <ListItem
                         image="/images/liked.png"
                         name="Liked songs"
@@ -26,8 +24,8 @@ export default async function Home() {
                 </div>
             </div>
         </Header>
-        <div className="mt-2 mb-7 px-6">
-            <div className="flex justify-between items-center">
+          <div className="mt-2 mb-7 px-6">
+              <div className="flex justify-between items-center">
                 <h1 className="text-white font-semibold text-xl">
                     Latest songs
                 </h1>
--- app/search/page.tsx
+++ app/search/page.tsx
@@ -15,7 +15,7 @@ const Search = async ({searchParams}: SearchProps) => {
     const songs = await getSongsByTitle(searchParams.title);
 
     return(
-        <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-hidden">
+        <div className="bg-neutral-900 rounded-lg h-full w-full overflow-hidden overflow-y-auto">
             <Header className="from-bg-neutral-900">
                 <div className="mb-2 flex flex-col gap-y-6">
                     <h1 className="text-3xl font-bold text-neon-pink">
